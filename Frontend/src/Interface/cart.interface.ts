d26d9d0d84df072f03cd9ff49b7400502f7228ff
gmshimon <simon.rosedale99@gmail.com>
2024-08-11 21:55:27 +0800

    redux impletemented

---
+++ Frontend/src/Interface/cart.interface.ts
@@ -0,0 +1,9 @@
+interface cartInterface {
+    name:string;
+    price:number;
+    variant:string;
+    description: string
+    photos: string[],
+    quantity: number;
+}
+export default cartInterface
--- Frontend/src/Pages/HomePage/HomePage.tsx
+++ Frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
+'use client'
 import Banner from '@/Components/Banner/Banner';
 import Category from '@/Components/Category/Category';
 import Reviews from '@/Components/Reivews/Reviews';
-import React from 'react';
+import { increment } from '@/lib/features/userSlice';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const HomePage = () => {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(increment()) 
+    },[])
     return (
         <div>
             <Banner/>
