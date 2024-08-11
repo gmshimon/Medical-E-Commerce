b4dacfa710276e728c358ddbd8c0fdb952014742
gmshimon <simon.rosedale99@gmail.com>
2024-08-12 05:20:45 +0800

    packages added

---
+++ Backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(cors());
+
+//parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app
