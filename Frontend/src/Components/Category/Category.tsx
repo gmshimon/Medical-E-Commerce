d634effb61a0534c5dd8f89a776939cdb2e8b5b7
gmshimon <simon.rosedale99@gmail.com>
2024-08-15 09:58:12 +0800

    fixed the home page

--- Frontend/src/Components/Category/Category.tsx
+++ Frontend/src/Components/Category/Category.tsx
@@ -6,13 +6,17 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategories } from "@/lib/features/categorySlice";
+import { RootState } from "@/lib/store";
 
 const Category = () => {
-  const [categories, setCategories] = useState([]);
+  const {
+    categories
+  } = useSelector((state: RootState) => state.category);
+  const dispatch = useDispatch()
   useEffect(() => {
-    fetch("categories.json")
-      .then((res) => res.json())
-      .then((data) => setCategories(data));
+    dispatch(getAllCategories());
   }, []);
   return (
     <div>
@@ -27,14 +31,32 @@ const Category = () => {
         pagination={{
           clickable: true,
         }}
+        breakpoints={{
+          '@0.00': {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          '@0.75': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          '@1.00': {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          '@1.50': {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
         modules={[Pagination]}
         className="mySwiper py-15"
       >
         {categories.map((category) => (
           <SwiperSlide  key={category.name}>
-            <div className="card bg-base-100 lg:w-80 shadow-xl">
+            <div className="card bg-base-100  w-[180px] h-[280px] md:w-80 lg:h-[350px] shadow-xl">
               <figure>
-                <img src={category?.thumbnail} alt="Shoes" />
+                <img className="w-[180px] h-[120px] md:w-[320px] md:h-[215px]" src={category?.thumbnail} alt="Shoes" />
               </figure>
               <div className="card-body">
                 <h2 className="font-bold">{category.name}</h2>
